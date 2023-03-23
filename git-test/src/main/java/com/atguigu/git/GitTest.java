2e172ec720949b922ed4591cc13f21646f44494e
wrystart <895437987@qq.com>
2023-03-23 17:12:47 +0800

    push commit2

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test1");
-        System.out.println("push test");
+        System.out.println("push test1");
     }
 }
